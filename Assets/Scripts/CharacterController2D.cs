2bfdc977834d5c2fb3b57619c4db6b3539a1d244
Michael Schroder <thormme@yahoo.com>
2019-10-05 00:56:13 -0700

    Added state manager and 2d character controller

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float right = Input.GetAxis("Horizontal");
+        float down = Input.GetAxis("Vertical");
+        gameObject.transform.Translate(right, down, 0);
+    }
+}
