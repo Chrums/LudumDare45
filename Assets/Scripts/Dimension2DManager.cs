7a7251073426e1bf20ba0cd2b6513a63dc0fc0ed
Michael Schroder <thormme@yahoo.com>
2019-10-06 16:50:57 -0700

    Added dinemnsion Manager

---
+++ Assets/Scripts/Dimension2DManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dimension2DManager : MonoBehaviour
+{
+    public enum Dimension
+    {
+        AxisX = 0,
+        AxisY,
+        AxisZ,
+        AxisT,
+    }
+
+    public Dimension HorizontalAxis = Dimension.AxisX;
+    public Dimension VerticalAxis = Dimension.AxisY;
+
+    void SetDimensions(Dimension horizontal, Dimension vertical)
+    {
+        HorizontalAxis = horizontal;
+        VerticalAxis = vertical;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
