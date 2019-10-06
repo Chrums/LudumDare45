59f3ed3bcbabd186f68d4c6b143d8382c41ceafd
Jake Chrumka <chrumka.mj@gmail.com>
2019-10-06 15:51:29 -0700

    Things

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,27 @@ namespace Fizz6.LudumDare45
     public static class Player
     {
         public static string Name = null;
+
         public static Texture2D Texture = null;
+
+        private static Sprite sprite = null;
+
+        public static Sprite Sprite
+        {
+            get
+            {
+                if (Texture == null)
+                {
+                    return null;
+                }
+
+                if (sprite == null)
+                {
+                    sprite = Sprite.Create(Texture, new Rect(Vector2.zero, new Vector2(Texture.width, Texture.height)), new Vector2(0.5f, 0.5f));
+                }
+
+                return sprite;
+            }
+        }
     }
 }
