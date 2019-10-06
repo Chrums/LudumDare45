502b419899d2c98ee77d93a62da03c77190fe4c2
Michael Schroder <thormme@yahoo.com>
2019-10-06 15:50:36 -0700

    Fixed states past 3 mins

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -17,8 +17,18 @@ public class CharacterController2D : MonoBehaviour
     void Update()
     {
         float right = Input.GetAxis("Horizontal");
-        var cast = Physics2D.BoxCast(gameObject.GetComponent<Rigidbody2D>().worldCenterOfMass, gameObject.GetComponent<BoxCollider2D>().size, 0, Vector2.down, 10, 1 << LayerMask.NameToLayer("World"));
-        bool willJump = Input.GetAxis("Vertical") > 0 && cast.rigidbody != null && cast.distance < gameObject.GetComponent<BoxCollider2D>().size.y / 2 + 0.02f && gameObject.GetComponent<Rigidbody2D>().velocity.y <= 0;
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(right * speed, willJump ? jump : gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        var cast = Physics2D.BoxCast(
+            gameObject.GetComponent<Rigidbody2D>().worldCenterOfMass,
+            gameObject.GetComponent<BoxCollider2D>().size,
+            0,
+            Vector2.down,
+            10,
+            1 << LayerMask.NameToLayer("World"));
+        bool willJump = Input.GetAxis("Vertical") > 0 && cast.rigidbody != null
+            && cast.distance < gameObject.GetComponent<BoxCollider2D>().size.y / 2 + 0.02f
+            && gameObject.GetComponent<Rigidbody2D>().velocity.y <= 0;
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(
+            right * speed,
+            willJump ? jump : gameObject.GetComponent<Rigidbody2D>().velocity.y);
     }
 }
